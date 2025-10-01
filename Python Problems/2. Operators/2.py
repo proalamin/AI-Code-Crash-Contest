503d3c37d35ad3afea2c5f5b9f4ff24506c4586e
proalamin <pro.alamin247@gmail.com>
2025-10-01 20:10:00 +0600

    solve 2 no problem in python Operators

---
+++ Python Problems/2. Operators/2.py
@@ -0,0 +1,11 @@
+# 2.Find the remainder when one number is divided by another.
+
+num1=input("enter the first number- ")
+num2=input("enter the second number- ")
+
+num1=int(num1)
+num2=int(num2)
+
+remainder= num1 % num2
+
+print("remainder is ", remainder)
