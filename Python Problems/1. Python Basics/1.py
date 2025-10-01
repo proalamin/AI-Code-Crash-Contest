cbc3f4fb6bf180165d33208c495a8f00fc7f59ab
proalamin <pro.alamin247@gmail.com>
2025-10-01 12:13:21 +0600

    solve 1 no problem in python basics

---
+++ Python Problems/1. Python Basics/1.py
@@ -0,0 +1,11 @@
+# 1.Take two numbers as input and print their sum.
+
+num1=input("enter num 1- ") 
+num2=input("enter num 2- ")
+
+num1=int(num1)
+num2=int(num2)
+
+sum= num1+num2
+
+print(sum)
--- README.md
+++
@@ -1,1 +0,0 @@
-# AI-Code-Crash-Contest
