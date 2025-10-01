b7ad907f015042317e457a11254c386423531573
proalamin <pro.alamin247@gmail.com>
2025-10-01 19:40:16 +0600

    solve 1 no problem in python Operators

---
+++ Python Problems/2. Operators/1.py
@@ -0,0 +1,17 @@
+num1=input("enter the first number- ")
+num2=input("enter the second number- ")
+
+num1=int(num1)
+num2=int(num2)
+
+add= num1+num2
+sub= num1-num2
+multi= num1*num2
+division= num1/num2
+
+print("addition is ", add)
+print("subtraction is ", sub)
+print("multiplication is ", multi)
+print("division is ", division)
+
+
