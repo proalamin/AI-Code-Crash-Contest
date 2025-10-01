37d1fe5dcb8447649fa52eaa7fc9d717c8a6e4ff
proalamin <pro.alamin247@gmail.com>
2025-10-01 12:33:44 +0600

    solve 3 no problem in python basics

---
+++ Python Problems/1. Python Basics/3.py
@@ -0,0 +1,9 @@
+# 3.Write a program to find the square and cube of a number.
+
+num=input("enter the number- ")
+num=float(num)
+
+square= num * num
+cube= num* num * num
+
+print(num,"-> square is ",square, "and", "cube is", cube)
