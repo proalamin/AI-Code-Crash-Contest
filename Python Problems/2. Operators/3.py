18419052c0e3adb619a627f7724f65529d3eee79
proalamin <pro.alamin247@gmail.com>
2025-10-02 13:08:41 +0600

    solve 3 no problem in python Operators

---
+++ Python Problems/2. Operators/3.py
@@ -0,0 +1,9 @@
+# 3.Take a number and check if it is even or odd using the modulus operator.
+
+num=input("enter the number-")
+num =int(num)
+
+if (num % 2==0):
+    print(num, "this number is even")
+else:
+    print(num, "this number is odd")
