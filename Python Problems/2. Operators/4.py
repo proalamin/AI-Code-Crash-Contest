262519a9e94a48a867581ae6e57296dfad95fad5
proalamin <pro.alamin247@gmail.com>
2025-10-02 13:21:02 +0600

    solve 4 no problem in python Operators

---
+++ Python Problems/2. Operators/4.py
@@ -0,0 +1,11 @@
+# 4.Demonstrate floor division (//) and power (**) operators with user input.
+
+
+num1=input("enter the first number- ")
+num2=input("enter the second number- ")
+
+num1=int(num1)
+num2=int(num2)
+
+print("floor division answer is- ", num1 // num2)
+print("power answer is- ", num1 ** num2)
